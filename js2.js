f73f9c8cb913c249a2641f8fce0776726eab98f5
Michaelstones <95419980+Michaelstones@users.noreply.github.com>
2023-01-23 22:41:33 +0100

    reverted

--- js2.js
+++ js2.js
@@ -2,13 +2,13 @@ const menu = document.querySelector('#menu')
 const headerClass =document.querySelector('header')
 const scrol = document.querySelector('.top')
 
-menu.addEventListener('click', (e) => {
-    e.stopPropagation()
+menu.addEventListener('click', () => {
+  
     menu.classList.toggle('fa-times')
     headerClass.classList.toggle('toggle')
 })
 
-document.addEventListener('scroll', (e) => {
+document.addEventListener('scroll', () => {
     e.stopPropagation()
     const scrollP = window.scrollY
     if(scrollP > 50){
